8b03834396d60b0d77eed90879b91ccc970698bf
NISHA GUPTA <nisha9119gupta@gmail.com>
2026-07-31 11:46:19 +0530

    added video and user model

---
+++ majorpro/src/db/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+import dns from "dns";
+
+if (process.env.NODE_ENV !== "production") {
+  dns.setServers(["1.1.1.1", "8.8.8.8"]);
+}
+
+
+const connectDB=async()=>{
+    try {
+       
+      const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      console.log(`\n mongodb connected !! DB HOST:${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB CONNECTION ERROR" , error);
+        process.exit(1)
+        
+    }
+}
+
+
+
+export default connectDB;
